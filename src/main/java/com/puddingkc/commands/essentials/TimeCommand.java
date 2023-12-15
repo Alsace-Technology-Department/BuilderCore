0bdbcfd7e459fb5cee1810767d6b0c80967835d6
Kyle Ma <m2069134445@icloud.com>
2023-12-16 04:30:09 +0800

    time set/add

--- src/main/java/com/puddingkc/commands/essentials/TimeCommand.java
+++ src/main/java/com/puddingkc/commands/essentials/TimeCommand.java
@@ -1,5 +1,6 @@
 package com.puddingkc.commands.essentials;
 
+import org.apache.commons.lang.StringUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.World;
@@ -17,7 +18,9 @@ import java.util.List;
 public class TimeCommand implements CommandExecutor, TabCompleter {
 
     private final String error = ChatColor.GRAY + "正确指令:\n§f/time <时间> §7- 设置你当前世界的时间\n§f/time <时间> <世界> §7- 设置指定世界的时间";
-    private static final List<String> times = Arrays.asList("sunrise", "day", "morning", "noon", "afternoon", "sunset", "night", "midnight", "5000");
+    private static final List<String> times1 = Arrays.asList("sunrise", "day", "morning", "noon", "afternoon", "sunset", "night", "midnight", "5000");
+
+    private static final List<String> times0 = Arrays.asList("sunrise", "day", "morning", "noon", "afternoon", "sunset", "night", "midnight", "5000", "set", "add");
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] strings) {
@@ -38,6 +41,38 @@ public class TimeCommand implements CommandExecutor, TabCompleter {
         }
 
         if (strings.length == 2) {
+            Player player = (Player) sender;
+            switch (strings[0]) {
+                case "set" -> {
+                    if (!player.hasPermission("alsace.commands.time")) {
+                        player.sendMessage(ChatColor.RED + "你没有使用该命令的权限");
+                        return false;
+                    }
+                    long timeTicks = getTimeFromKeyword(strings[1]);
+                    if (timeTicks == -1) {
+                        sender.sendMessage(error);
+                        return false;
+                    }
+                    player.getWorld().setTime(timeTicks);
+                    player.sendMessage(ChatColor.GRAY + "已将当前世界时间设置为 §f" + strings[1] + " ticks");
+                    return true;
+                }
+                case "add" -> {
+                    if (!player.hasPermission("alsace.commands.time")) {
+                        player.sendMessage(ChatColor.RED + "你没有使用该命令的权限");
+                        return false;
+                    }
+                    if (!StringUtils.isNumeric(strings[1])) {
+                        sender.sendMessage(error);
+                        return false;
+                    }
+                    long var = Long.parseLong(strings[1]);
+                    long var1 = player.getWorld().getTime();
+                    player.getWorld().setTime(var + var1);
+                    player.sendMessage(ChatColor.GRAY + "已将当前世界时间增加 §f" + var + " ticks");
+                    return true;
+                }
+            }
             if (!sender.hasPermission("alsace.commands.time.other")) {
                 sender.sendMessage(ChatColor.RED + "你没有使用该命令的权限");
                 return false;
@@ -62,35 +97,52 @@ public class TimeCommand implements CommandExecutor, TabCompleter {
 
     private long getTimeFromKeyword(String keyword) {
         switch (keyword.toLowerCase()) {
-            case "sunrise":
+            case "sunrise" -> {
                 return 23000;
-            case "day":
+            }
+            case "day" -> {
                 return 0;
-            case "morning":
+            }
+            case "morning" -> {
                 return 1000;
-            case "noon":
+            }
+            case "noon" -> {
                 return 6000;
-            case "afternoon":
+            }
+            case "afternoon" -> {
                 return 9000;
-            case "sunset":
+            }
+            case "sunset" -> {
                 return 12000;
-            case "night":
+            }
+            case "night" -> {
                 return 14000;
-            case "midnight":
+            }
+            case "midnight" -> {
                 return 18000;
-            default:
+            }
+            default -> {
                 try {
                     return Long.parseLong(keyword);
                 } catch (NumberFormatException e) {
                     return -1;
                 }
+            }
         }
     }
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String s, String[] strings) {
         if (strings.length == 1 && sender.hasPermission("alsace.commands.time")) {
-            return times;
+            return times0;
+        }
+        if (strings.length == 2 && strings[0].equals("set") && sender.hasPermission("alsace.commands.time")) {
+            return times1;
+        }
+        if (strings.length == 2 && strings[0].equals("add") && sender.hasPermission("alsace.commands.time")) {
+            List<String> list = new ArrayList<>();
+            list.add("1000");
+            return list;
         }
         if (strings.length == 2 && sender.hasPermission("alsace.commands.time.other")) {
             List<String> list = new ArrayList<>();
